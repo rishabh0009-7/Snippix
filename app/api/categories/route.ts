32f2f48244532005099b5491b89ac6503dbccc6a
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-07-28 15:41:56 +0530

    added category api route

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,9 +1,12 @@
 import { auth } from "@clerk/nextjs/server";
 import { createErrorResponse } from "@/src/lib/utils/helper";
-import { connect } from "http2";
+import { createResponse } from "@/src/lib/utils/helper";
 import connectToDatabase from "@/src/lib/mongodb";
 import User from "@/src/lib/models/User";
 import Category from "@/src/lib/models/Category";
+import { createCategorySchema } from "@/src/lib/validations/snippet";
+
+
 export const async function GET(){
 
     try {
@@ -21,21 +24,41 @@ export const async function GET(){
             return createErrorResponse('user not found', 404)
         }
 
-        const category = await Category.find({userrId: user._id})
+const body = await request.json()
+        const validateData = createCategorySchema.parse(body)
 
-        
-
-
-
-
+        const existingcategory = await Category.find({userrId: user._id,
+            name :validateData.name
 
 
 
-        
-    } catch (error) {
+        })
+        if(existingcategory){
+            return createErrorResponse('Category with this name already exists', 409);
+
+        }
+
+        const category = new Category({
+            ...validateData,
+            userId :user._id
+
+        })
+
+        await category.save()
+
+
+    return createResponse({ category }, 201);
+
+} catch (error) {
+    console.error('POST category error:', error);
+    if (error.name === 'ZodError') {
+      return createErrorResponse('Invalid input data', 400);
+    }
+    return createErrorResponse('Failed to create category', 500);
+  }
         
     }
 
 
-}
+
     
